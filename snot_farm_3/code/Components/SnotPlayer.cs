64534a704fa3ac0d74ce5ae5eeef60894d91d27b
meotis <rsakelr@gmail.com>
2024-03-04 03:11:20 +0300

    Add properties to SnotPlayer.cs

--- snot_farm_3/code/Components/SnotPlayer.cs
+++ snot_farm_3/code/Components/SnotPlayer.cs
@@ -1,9 +1,33 @@
-using Sandbox;
+using Sandbox.Citizen;
 
 public sealed class SnotPlayer : Component
 {
+	[Property]
+	 public GameObject Camera { get; set; }
+
+
+	[Property]
+	public CitizenAnimationHelper AnimationHelper { get; set; }
+
+	[Property]
+	[Range(0f, 400f, 1f)]
+	public float WalkSpeed { get; set; } = 120f;
+
+	[Property]
+	[Range(0f, 800f, 1f)]
+	public float RunSpeed { get; set; } = 250f;
+
+	[Property]
+	[Range(0f, 1000f, 10f)]
+	public float JumpStrength { get; set; } = 400f;
+
 	protected override void OnUpdate()
 	{
 
+	}
+
+	protected override void OnFixedUpdate()
+	{
+		base.OnFixedUpdate();
 	}
 }
